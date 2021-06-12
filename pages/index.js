be23309b5b1137fa62d8ab1da1800e7355fd0638
Iron Shark Adi <ironsharkadi2020@gmail.com>
2021-06-12 12:45:12 +0600

    first

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+// import Image from 'next/image'
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Hulu Is</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            
+            <div >
+                <h1>Hello world</h1>
+            </div>
+        </>
+  )
+}

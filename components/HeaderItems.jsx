f00b7c7470f23c34a297bfeac7b806fd835e3ab0
Iron Shark Adi <ironsharkadi2020@gmail.com>
2021-06-13 02:52:18 +0600

    2 component

--- components/HeaderItems.jsx
+++ components/HeaderItems.jsx
@@ -2,7 +2,7 @@ function HeaderItems({Icon , title}) {
     return (
         <div className="group">
             <Icon className="h-8 mb-1" />
-            <p className="tracking-widest opacity-0 group-hover:opacity-100">{title}</p>
+            <p className="tracking-widest opacity-0 group-hover:opacity-100  ">{title}</p>
         </div>
     )
 }
